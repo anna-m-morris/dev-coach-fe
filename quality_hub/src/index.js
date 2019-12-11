35c126a2abce11a3f3540441c6be1d2edf044dfa
BenjaminGrabow <codingbenjamingrabow@gmx.de>
2019-12-11 19:02:08 +0100

    connect redux and react router with react

--- quality_hub/src/index.js
+++ quality_hub/src/index.js
@@ -1,6 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter as Router } from "react-router-dom";
+import rootReducer from "./store/reducers";
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware } from "redux";
+import thunk from "redux-thunk";
+import logger from "redux-logger";
+import "./index.css";
+import App from "./App";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+store = createStore(rootReducer, applyMiddleware(logger, thunk));
+
+ReactDOM.render(
+  <Provider store={store}>
+    <Router>
+      <App />
+    </Router>
+  </Provider>,
+  document.getElementById("root")
+);
