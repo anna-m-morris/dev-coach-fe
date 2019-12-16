e98131b83f85a9ba833b357a73927cf499ce9287
Dom <domeccleston@gmail.com>
2019-12-16 15:53:46 +0000

    added loading state for login

--- quality_hub/src/state/actions/actionCreators.js
+++ quality_hub/src/state/actions/actionCreators.js
@@ -1,17 +1,17 @@
-import * as types from './actionTypes';
-import axios from 'axios';
+import * as types from "./actionTypes";
+import axios from "axios";
 
-export const login = (url, props, values) => (dispatch) => {
-    console.log(url, props, values);
-    dispatch({ type: types.LOGIN_START });
-    axios
-      .post(url, values)
-      .then(res => {
-        dispatch({ type: types.LOGIN_SUCCESSFUL });
-        localStorage.setItem("token", res.data.token);
-        props.history.push("/dashboard");
-      })
-      .catch(err => {
-        dispatch({ type: types.LOGIN_ERROR, payload: err });
-      });
-  };
+export const login = (url, props, values) => dispatch => {
+  console.log(url, props, values);
+  dispatch({ type: types.LOGIN_START });
+  axios
+    .post(url, values)
+    .then(res => {
+      dispatch({ type: types.LOGIN_SUCCESSFUL });
+      localStorage.setItem("token", res.data.token);
+      props.history.push("/dashboard");
+    })
+    .catch(err => {
+      dispatch({ type: types.LOGIN_ERROR, payload: err });
+    });
+};
