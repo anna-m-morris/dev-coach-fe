d80f2df8d71022324cde39924ed8a15724fbb2fc
Jayne Carmichael Norrie <39460834+jaynecn@users.noreply.github.com>
2019-12-19 13:46:06 +0000

    added dummy data

--- quality_hub/src/components/Marketplace.jsx
+++ quality_hub/src/components/Marketplace.jsx
@@ -2,6 +2,50 @@ import React from 'react';
 import styled from 'styled-components';
 import devices from './devices';
 
+// DUMMY DATA //
+ const marketplaceUsers = [{
+  first_name: 'Jayne',
+  last_name: 'Carmichael Norrie',
+  email: 'jayne@musicisourforte.co.uk',
+  password: 'chico',
+  role_id: 2,
+},
+{
+  first_name: 'Liam',
+  last_name: 'Sutton',
+  email: 'liam@google.com',
+  password: 'liam',
+  role_id: 2,
+},
+{
+  first_name: 'Funmi',
+  last_name: 'Talabi',
+  email: 'funmi@google.com',
+  password: 'funmi',
+  role_id: 2,
+}];
+
+const marketplaceCoaches = [{
+  user_id: 1,
+  avatar_url: 'google.com',
+  experience_level: 1,
+  skill_level: 1,
+  description:
+    'Jayne worked as a singing teacher for 9 years and is now studying with Lambda School',
+},
+{
+  user_id: 2,
+  avatar_url: 'Github.com',
+  experience_level: 1,
+  skill_level: 1,
+},
+{
+  user_id: 3,
+  avatar_url: 'Paypal.com',
+  experience_level: 1,
+  skill_level: 1,
+}];
+
 
 // STYLED COMPONENTS //
 const BackgroundContainer = styled.div`
