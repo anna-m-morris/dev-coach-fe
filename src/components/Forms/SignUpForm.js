e131ac479abc4372f04770c072f84df2febf27d7
Liam Sutton <devliam90@gmail.com>
2019-12-20 09:52:17 +0000

    add error handling to signup form

--- src/components/Forms/SignUpForm.js
+++ src/components/Forms/SignUpForm.js
@@ -4,7 +4,7 @@ import styled from 'styled-components';
 import { withFormik, Form, Field } from 'formik';
 import * as Yup from 'yup';
 
-import { StyledButton, buttonTheme, invertTheme } from '../Landing';
+import { StyledButton, buttonTheme } from '../Landing';
 import {
   GreyBackgroundContainer,
   FormCard,
@@ -32,7 +32,7 @@ const RegisterCard = styled(FormCard)`
   }
 `;
 
-function SignUpForm(props) {
+function SignUpForm({ isSubmitting, errors, touched }) {
   return (
     <GreyBackgroundContainer>
       <RegisterCard>
@@ -40,30 +40,60 @@ function SignUpForm(props) {
         <FormContainer>
           <Form>
             <ShortInputContainer>
-              <Field
-                type='text'
-                name='first_name'
-                placeholder='First name'
-              />
-              <Field
-                type='text'
-                name='last_name'
-                placeholder='Last name'
-              />
+              <div>
+                {errors.firstName && touched.firstName && (
+                  <p>{errors.firstName}</p>
+                )}
+                <Field
+                  type='text'
+                  name='firstName'
+                  placeholder='First name'
+                />
+              </div>
+              <div>
+                {' '}
+                {errors.lastName && touched.lastName && (
+                  <p>{errors.lastName}</p>
+                )}
+                <Field
+                  type='text'
+                  name='lastName'
+                  placeholder='Last name'
+                />
+              </div>
             </ShortInputContainer>
-            <Field type='email' name='email' placeholder='Email' />
-            <Field
-              type='password'
-              name='password'
-              placeholder='Password'
-            />
-            <Field
-              type='password'
-              name='confirm_password'
-              placeholder='Confirm Password'
-            />
+            <div>
+              {errors.email && touched.email && <p>{errors.email}</p>}
+              <Field type='email' name='email' placeholder='Email' />
+            </div>
+            <div>
+              {' '}
+              {errors.password && touched.password && (
+                <p>{errors.password}</p>
+              )}
+              <Field
+                type='password'
+                name='password'
+                placeholder='Password'
+              />
+            </div>
+            <div>
+              {' '}
+              {errors.confirmPassword && touched.confirmPassword && (
+                <p>{errors.confirmPassword}</p>
+              )}
+              <Field
+                type='password'
+                name='confirmPassword'
+                placeholder='Confirm Password'
+              />
+            </div>
 
-            <StyledButton theme={buttonTheme} type='submit'>
+            <StyledButton
+              disabled={isSubmitting}
+              theme={buttonTheme}
+              type='submit'
+            >
               Get Started
             </StyledButton>
           </Form>
@@ -75,34 +105,43 @@ function SignUpForm(props) {
 
 const FormikSignUpForm = withFormik({
   mapPropsToValues({
-    first_name,
-    last_name,
+    firstName,
+    lastName,
     email,
     password,
-    confirm_password,
+    confirmPassword,
   }) {
     return {
-      first_name: first_name || '',
-      last_name: last_name || '',
+      firstName: firstName || '',
+      lastName: lastName || '',
       email: email || '',
       password: password || '',
-      confirm_password: confirm_password || '',
+      confirmPassword: confirmPassword || '',
     };
   },
   validationSchema: Yup.object().shape({
-    first_name: Yup.string().required('Please enter your first name'),
-    last_name: Yup.string().required('Please enter your last name'),
+    firstName: Yup.string().required('Please enter your first name'),
+    lastName: Yup.string().required('Please enter your last name'),
+    email: Yup.string()
+      .email('Please enter a valid email')
+      .required('Please enter your email'),
     password: Yup.string()
       .required('Please enter your password')
-      .min(6),
-    confirm_password: Yup.string().oneOf(
-      [Yup.ref('password'), null],
-      "Your passwords don't match",
-    ),
+      .min(
+        6,
+        'Please make sure your password is 6 characters or longer',
+      ),
+    confirmPassword: Yup.string()
+      .oneOf(
+        [Yup.ref('password'), null],
+        "Your passwords don't match",
+      )
+      .required('Please confirm your password'),
   }),
 
-  handleSubmit(values, { props }) {
-    console.log(values);
+  handleSubmit(values, { props, setSubmitting, resetForm }) {
+    resetForm();
+    setSubmitting(false);
 
     props.register(props, values);
   },
