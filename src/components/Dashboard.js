ec47c7c627748e1e49e0599310fd99e94e8d9286
BenjaminGrabow <codingbenjamingrabow@gmx.de>
2019-12-23 12:01:52 +0100

    delete unecessary code

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -189,22 +189,22 @@ function Dashboard(props) {
         <div className={classes.appBarSpacer} />
         <Container maxWidth='lg' className={classes.container}>
           <Grid container spacing={3}>
-            {/* Chart */}
+          
             <Grid item xs={12} md={8} lg={9}>
               <Paper className={fixedHeightPaper}>
-                {/* <Chart /> */}
+              
               </Paper>
             </Grid>
-            {/* Recent Deposits */}
+           
             <Grid item xs={12} md={4} lg={3}>
               <Paper className={fixedHeightPaper}>
-                {/* <Deposits /> */}
+              
               </Paper>
             </Grid>
-            {/* Recent Orders */}
+           
             <Grid item xs={12}>
               <Paper className={classes.paper}>
-                {/* <Orders /> */}
+              
               </Paper>
             </Grid>
           </Grid>
