0f007b1d690e7756587c9f48b764e542019f8908
BenjaminGrabow <codingbenjamingrabow@gmx.de>
2019-12-24 16:53:40 +0100

    added Switch routes to app.js

--- src/App.js
+++ src/App.js
@@ -1,17 +1,25 @@
 import React from 'react';
-import { Route } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 import { connect } from 'react-redux';
 
 import PrivateRoute from './utils/PrivateRoute';
 import LoginForm from './components/Forms/LoginForm';
 import SignUpForm from './components/Forms/SignUpForm';
 import Dashboard from './components/Dashboard';
+import UserDashboard from './views/UserDashboard/UserDashboard';
+import Marketplace from './components/Marketplace';
 import Landing from './components/Landing';
 import InterviewerForm from './components/Forms/InterviewerForm';
 import StudentForm from './components/Forms/StudentForm';
 import UserTypePage from './components/UserType/UserTypePage';
 
 function App() {
+  const routes = (
+    <Switch>
+      <PrivateRoute path={'/dashboard'} component={UserDashboard} />
+      <PrivateRoute path={'/marketplace'} component={Marketplace} />
+    </Switch>
+  );
   return (
     <>
       <Route exact path='/' component={Landing} />
@@ -20,7 +28,10 @@ function App() {
       <Route path='/interviewer' component={InterviewerForm} />
       <Route path='/student' component={StudentForm} />
       <Route path='/user/type' component={UserTypePage} />
-      <PrivateRoute path='/dashboard' component={Dashboard} />
+      <PrivateRoute
+        path='/dashboard'
+        render={() => <Dashboard routes={routes} />}
+      />
     </>
   );
 }
