d75cbdac313f5d50c9827683babc228f6abd3501
BenjaminGrabow <codingbenjamingrabow@gmx.de>
2019-12-25 20:01:58 +0100

    add calendar to route

--- src/App.js
+++ src/App.js
@@ -12,13 +12,14 @@ import Landing from './components/Landing';
 import InterviewerForm from './components/Forms/InterviewerForm';
 import StudentForm from './components/Forms/StudentForm';
 import UserTypePage from './components/UserType/UserTypePage';
+import Calendar from './components/Calendar/Calendar';
 
 function App(props) {
   const routes = (
     <Switch>
       <Route path={'/dashboard'} component={UserDashboard} />
       <Route path={'/marketplace'} component={Marketplace} />
-      <Route path={'/faq'} component={Marketplace} />
+      <Route path={'/faq'} component={Calendar} />
       <Route path={'/feedback'} component={Marketplace} />
       <Route path={'/settings'} component={Marketplace} />
     </Switch>
--- src/components/Calendar/Calendar.js
+++ src/components/Calendar/Calendar.js
@@ -1,20 +1,20 @@
 import React, { Component } from 'react';
 import Calendar from 'react-calendar';
- 
-class MyApp extends Component {
+
+export default class MyApp extends Component {
   state = {
     date: new Date(),
-  }
- 
-  onChange = date => this.setState({ date })
- 
+  };
+
+  onChange = date => {
+    this.setState({ date });
+    console.log(date);
+  };
+
   render() {
     return (
       <div>
-        <Calendar
-          onChange={this.onChange}
-          value={this.state.date}
-        />
+        <Calendar onChange={this.onChange} value={this.state.date} />
       </div>
     );
   }
