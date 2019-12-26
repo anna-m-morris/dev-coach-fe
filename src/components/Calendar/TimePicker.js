d5ebd204b95d75d9f785370baf07531ca94e7f2b
BenjaminGrabow <codingbenjamingrabow@gmx.de>
2019-12-26 12:35:57 +0100

    code indentation and npm i

--- src/components/Calendar/TimePicker.js
+++ src/components/Calendar/TimePicker.js
@@ -3,34 +3,25 @@ import React from 'react';
 import Grid from '@material-ui/core/Grid';
 import DateFnsUtils from '@date-io/date-fns';
 import {
-      MuiPickersUtilsProvider,
-    KeyboardTimePicker,
+  MuiPickersUtilsProvider,
+  KeyboardTimePicker,
 } from '@material-ui/pickers';
 
 export default function TimePicker(props) {
-    // const [selectedDate, setSelectedDate] = React.useState(new Date('2014-08-18T21:11:54'));
-
-    // const handleDateChange = date => {
-    //     setSelectedDate(date);
-    //     console.log(date)
-    // };
-
-    return (
-        <MuiPickersUtilsProvider
-         utils={DateFnsUtils}
-         >
-        <Grid container justify="space-around">
-            <KeyboardTimePicker
-                margin="normal"
-                id="time-picker"
-                label="Time picker"
-                value={props.date}
-                onChange={props.saveDayTime}
-                KeyboardButtonProps={{
-                    'aria-label': 'change time',
-                }}
-            />
-        </Grid>
-        </MuiPickersUtilsProvider>
-    );
+  return (
+    <MuiPickersUtilsProvider utils={DateFnsUtils}>
+      <Grid container justify='space-around'>
+        <KeyboardTimePicker
+          margin='normal'
+          id='time-picker'
+          label='Time picker'
+          value={props.date}
+          onChange={props.saveDayTime}
+          KeyboardButtonProps={{
+            'aria-label': 'change time',
+          }}
+        />
+      </Grid>
+    </MuiPickersUtilsProvider>
+  );
 }
