60fc0aa4f20d4f80923ba3f1fc6e795bab1ade6c
Benjamin Grabow <benjamin_grabow@gmx.de>
2019-12-31 18:03:15 +0100

    delete comments and unecessary code

--- src/components/Calendar/Select.js
+++ src/components/Calendar/Select.js
@@ -1,92 +1,8 @@
-// import React from 'react';
-// import { makeStyles } from '@material-ui/core/styles';
-// import InputLabel from '@material-ui/core/InputLabel';
-// import MenuItem from '@material-ui/core/MenuItem';
-// import FormControl from '@material-ui/core/FormControl';
-// import Select from '@material-ui/core/Select';
-// import Button from '@material-ui/core/Button';
-
-// const useStyles = makeStyles(theme => ({
-//     button: {
-//         display: 'block',
-//         marginTop: theme.spacing(2),
-//     },
-//     formControl: {
-//         margin: theme.spacing(1),
-//         minWidth: 120,
-//     },
-// }));
-
-// export default function ControlledOpenSelect() {
-//     const classes = useStyles();
-//     const [age, setAge] = React.useState('');
-//     const [open, setOpen] = React.useState(false);
-
-//     const handleChange = event => {
-//         setAge(event.target.value);
-//     };
-
-//     const handleClose = () => {
-//         setOpen(false);
-//     };
-
-//     const handleOpen = () => {
-//         setOpen(true);
-//     };
-
-//     return (
-//         <div>
-//             <FormControl className={classes.formControl}>
-//                 <InputLabel id="demo-controlled-open-select-label">Length</InputLabel>
-//                 <Select
-//                     labelId="demo-controlled-open-select-label"
-//                     id="demo-controlled-open-select"
-//                     open={open}
-//                     onClose={handleClose}
-//                     onOpen={handleOpen}
-//                     value={null}
-//                     onChange={handleChange}
-//                 >
-//                     <MenuItem value="">
-//                         <em>None</em>
-//                     </MenuItem>
-//                     <MenuItem value={10}>30 Minutes</MenuItem>
-//                     <MenuItem value={20}>1 Hour</MenuItem>
-//                 </Select>
-//             </FormControl>
-
-//             <FormControl className={classes.formControl}>
-//                 <InputLabel id="demo-controlled-open-select-label">Topic</InputLabel>
-//                 <Select
-//                     labelId="demo-controlled-open-select-label"
-//                     id="demo-controlled-open-select"
-//                     open={open}
-//                     onClose={handleClose}
-//                     onOpen={handleOpen}
-//                     value={null}
-//                     onChange={handleChange}
-//                 >
-//                     <MenuItem value="">
-//                         <em>None</em>
-//                     </MenuItem>
-//                     <MenuItem value={1}>Frontend</MenuItem>
-//                     <MenuItem value={2}>Backend</MenuItem>
-//                     <MenuItem value={3}>Algortihms / Data Structes</MenuItem>
-//                     <MenuItem value={4}>Behavorial Interview</MenuItem>
-//                     <MenuItem value={5}>System Design</MenuItem>
-//                     <MenuItem value={6}>React</MenuItem>
-//                 </Select>
-//             </FormControl>
-//         </div>
-//     );
-// }
-
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import InputLabel from '@material-ui/core/InputLabel';
 import Input from '@material-ui/core/Input';
 import MenuItem from '@material-ui/core/MenuItem';
-import ListSubheader from '@material-ui/core/ListSubheader';
 import FormControl from '@material-ui/core/FormControl';
 import Select from '@material-ui/core/Select';
 
@@ -119,15 +35,6 @@ export default function GroupedSelect() {
 
     return (
         <div>
-            {/* <FormControl className={classes.formControl}>
-                <InputLabel htmlFor="grouped-native-select">Length</InputLabel>
-                <Select native defaultValue=""
-                    input={<Input onChange={handleChange} name="length-select" id="length-select" />}>
-                    <MenuItem value={1}>30 Minutes</MenuItem>
-                    <MenuItem value={2}>1 Hour</MenuItem>
-                </Select>
-            </FormControl> */}
-
             <FormControl className={classes.formControl}>
                 <InputLabel htmlFor="grouped-select">Length</InputLabel>
                 <Select defaultValue="Frontend" input={<Input onChange={handleChange} name="length-select" id="length-select" />}>
