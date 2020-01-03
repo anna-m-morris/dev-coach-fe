cbcca6de4eb1fa58cf1cfb57d9a8d061c2797e35
BenjaminGrabow <codingbenjamingrabow@gmx.de>
2020-01-03 22:24:54 +0100

    connect bookingReducer to store

--- src/state/reducers/index.js
+++ src/state/reducers/index.js
@@ -2,11 +2,13 @@ import { combineReducers } from 'redux';
 // import your reducers here
 import userReducer from './authentication';
 import appointmentsReducer from './appointments';
+import bookingReducer from './booking';
 
 const appReducer = combineReducers({
   // add your reducers here
   userReducer,
   appointmentsReducer,
+  bookingReducer,
 });
 
 /* const rootReducer = (state, action) => {
