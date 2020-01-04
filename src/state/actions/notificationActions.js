cea9dfbf43e0b11bfc0dfba71dcd8eb8650688da
BenjaminGrabow <codingbenjamingrabow@gmx.de>
2020-01-04 16:57:28 +0100

    add actioncreators for notifications

---
+++ src/state/actions/notificationActions.js
@@ -0,0 +1,15 @@
+export const SHOW_ERROR_MESSAGE = 'SHOW_ERROR_MESSAGE';
+export const SHOW_SUCCESS_MESSAGE = 'SHOW_SUCCESS_MESSAGE';
+export const SHOW_INFO_MESSAGE = 'SHOW_INFO_MESSAGE';
+
+export const showSuccessMessage = () => {
+  return { type: SHOW_SUCCESS_MESSAGE };
+};
+
+export const showErrorMessage = () => {
+  return { type: SHOW_ERROR_MESSAGE };
+};
+
+export const showInfoMessage = () => {
+  return { type: SHOW_INFO_MESSAGE };
+};
