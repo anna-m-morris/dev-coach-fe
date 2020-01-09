282eb7afb07ca49554e059c9d3ce8a54bf5cdb8b
BenjaminGrabow <codingbenjamingrabow@gmx.de>
2020-01-09 11:50:03 +0100

    create Feedback component

---
+++ src/views/Feedback/Feedback.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import styled from 'styled-components';
+
+
+const Feedback = () => {
+  return (  );
+}
+ 
+export default Feedback;
