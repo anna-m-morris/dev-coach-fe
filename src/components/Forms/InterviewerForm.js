55eaad1d2ec874d5263617f41b25c4f11a840ff2
Dom <domeccleston@gmail.com>
2020-01-09 13:00:23 +0000

    coach put request fixed

--- src/components/Forms/InterviewerForm.js
+++ src/components/Forms/InterviewerForm.js
@@ -1,32 +1,26 @@
 import React from 'react';
-
 import { connect } from 'react-redux';
-
 import styled from 'styled-components';
 import { withFormik, Form, Field } from 'formik';
 import * as Yup from 'yup';
-
+import { chooseUserRole } from '../../state/actions/authenticationActions';
 import { StyledButton, buttonTheme } from '../Landing';
 import {
   GreyBackgroundContainer,
   FormCard,
   FormContainer,
 } from './LoginForm';
-
 const RegisterCard = styled(FormCard)`
   width: 30em;
   height: 35em;
   font-family: ABeeZee;
-
   h1 {
     font-size: 24px;
   }
 `;
-
 const ThisGreyBackgroundContainer = styled(GreyBackgroundContainer)`
   font-family: ABeeZee;
 `;
-
 function StudentForm(props) {
   const handleUserRoleSubmit = () => {};
   return (
@@ -35,48 +29,62 @@ function StudentForm(props) {
         <h1> Interviewer Form </h1>
         <FormContainer>
           <Form>
-            <Field type='text' name='city' />
-            <Field
-              type='text'
-              name='level_of_experience'
-              placeholder=' select level of experience '
-            />
-            <Field type='text' name='skills' />
-            <Field type='text' name='description' />
-            <StyledButton
-              theme={buttonTheme}
-              onClick={handleUserRoleSubmit}
-            >
-              {' '}
-              Submit{' '}
-            </StyledButton>
+            <div>
+              <Field type='text' name='city' placeholder='Location' />
+            </div>
+            <div>
+              <Field
+                type='text'
+                name='experience'
+                placeholder='Select Level Of Experience'
+              />
+            </div>
+            <div>
+              <Field type='text' name='skills' placeholder='Skills' />
+            </div>
+            <div>
+              <Field
+                type='text'
+                name='description'
+                placeholder='Description'
+              />
+            </div>
+            <div>
+              <StyledButton
+                theme={buttonTheme}
+                onClick={handleUserRoleSubmit}
+                type='submit'
+              >
+                {' '}
+                Submit{' '}
+              </StyledButton>
+            </div>
           </Form>
         </FormContainer>
       </RegisterCard>
     </ThisGreyBackgroundContainer>
   );
 }
-
 const FormikStudentForm = withFormik({
+  mapPropsToValues({ city, experience, skills, description }) {
+    return {
+      city: city || '',
+      experience: experience || '',
+      skills: skills || '',
+      description: description || '',
+    };
+  },
   validationSchema: Yup.object().shape({
-    city: Yup.array(),
-    level_of_experience: Yup.array(),
-    skills: Yup.array(),
-    value: Yup.string().required(),
+    city: Yup.string(),
+    experience: Yup.string(),
+    skills: Yup.string(),
+    description: Yup.string().required(),
   }),
-  mapPropsToValues: props => ({
-    city: 'Location',
-    level_of_experience: 'Experience',
-    skills: 'Skills',
-    description: 'Describe yourself in one line',
-  }),
-  handleSubmit(values, { props }) {
-    props.register(
-      'http://localhost:5000/user/register',
-      props,
-      values,
-    );
+  handleSubmit(values, { resetForm, props }) {
+    resetForm();
+    props.chooseUserRole(props, values, 1);
   },
 })(StudentForm);
-
-export default connect(state => state)(FormikStudentForm);
+export default connect(state => state, { chooseUserRole })(
+  FormikStudentForm,
+);
