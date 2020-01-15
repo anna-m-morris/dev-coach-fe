c28697b8d083103ba84537a1dcaf09b614a3c2c0
BenjaminGrabow <codingbenjamingrabow@gmx.de>
2020-01-15 23:08:43 +0100

    create feedback input

--- src/components/Inputs/FeedbackInput.js
+++ src/components/Inputs/FeedbackInput.js
@@ -23,71 +23,8 @@ export default function MultilineTextFields() {
     <form className={classes.root} noValidate autoComplete='off'>
       <div>
         <TextField
-          id='standard-multiline-flexible'
-          label='Multiline'
-          multiline
-          rowsMax='4'
           value={value}
           onChange={handleChange}
-        />
-        <TextField
-          id='standard-textarea'
-          label='Multiline Placeholder'
-          placeholder='Placeholder'
-          multiline
-        />
-        <TextField
-          id='standard-multiline-static'
-          label='Multiline'
-          multiline
-          rows='4'
-          defaultValue='Default Value'
-        />
-      </div>
-      <div>
-        <TextField
-          id='filled-multiline-flexible'
-          label='Multiline'
-          multiline
-          rowsMax='4'
-          value={value}
-          onChange={handleChange}
-          variant='filled'
-        />
-        <TextField
-          id='filled-textarea'
-          label='Multiline Placeholder'
-          placeholder='Placeholder'
-          multiline
-          variant='filled'
-        />
-        <TextField
-          id='filled-multiline-static'
-          label='Multiline'
-          multiline
-          rows='4'
-          defaultValue='Default Value'
-          variant='filled'
-        />
-      </div>
-      <div>
-        <TextField
-          id='outlined-multiline-flexible'
-          label='Multiline'
-          multiline
-          rowsMax='4'
-          value={value}
-          onChange={handleChange}
-          variant='outlined'
-        />
-        <TextField
-          id='outlined-textarea'
-          label='Multiline Placeholder'
-          placeholder='Placeholder'
-          multiline
-          variant='outlined'
-        />
-        <TextField
           id='outlined-multiline-static'
           label='Multiline'
           multiline
--- src/views/Feedback/GiveFeedback.js
+++ src/views/Feedback/GiveFeedback.js
@@ -2,6 +2,7 @@ import React from 'react';
 import styled from 'styled-components';
 import { getFeedback } from '../../state/actions/feedbackActions';
 import GiveRating from '../../components/DataVisualization/GiveRating';
+import FeedbackInput from '../../components/Inputs/FeedbackInput';
 
 const StyledGiveFeedback = styled.div``;
 
@@ -9,6 +10,7 @@ export default function GiveFeedback() {
   return (
     <StyledGiveFeedback>
       <GiveRating />
+      <FeedbackInput />
     </StyledGiveFeedback>
   );
 }
