c5859333777efd098cf75b4f6b2416556532f657
BenjaminGrabow <codingbenjamingrabow@gmx.de>
2020-01-15 22:53:15 +0100

    create givefeedback component

---
+++ src/views/Feedback/GiveFeedback.js
@@ -0,0 +1,9 @@
+import react from 'react';
+import styled from 'styled-components';
+import { getFeedback } from '../../state/actions/feedbackActions';
+
+const StyledGiveFeedback = styled.div``;
+
+export default function GiveFeedback() {
+  return <StyledGiveFeedback></StyledGiveFeedback>;
+}
