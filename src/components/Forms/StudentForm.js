fb15daccd9a2832be559e05323e93a9b1c2ca0e3
Dom <domeccleston@gmail.com>
2020-01-15 14:41:06 +0000

    restyle form with more space

--- src/components/Forms/StudentForm.js
+++ src/components/Forms/StudentForm.js
@@ -47,18 +47,22 @@ const ThisGreyBackgroundContainer = styled(GreyBackgroundContainer)`
 
 const StudentCard = styled(FormCard)`
   display: flex;
+  width: 50%;
   h1 {
     margin: 0;
     margin-top: -0.2em;
     margin-bottom: 0.1em;
   }
 `;
-const StudentFormContainer = styled(FormContainer)`
+
+const StudentFormContainer = styled.div`
+  height: 100%;
+  width: 100%;
   display: flex;
   justify-content: center;
   align-items: center;
   flex-direction: column;
-  margin-top: -2em;
+  margin-top: -3.5em;
 `;
 
 const FormButton = styled(StyledButton)`
@@ -69,21 +73,20 @@ const FormButton = styled(StyledButton)`
 const InfoParagraph = styled.p`
   width: 85%;
   text-align: center;
-  margin-top: 0.4em;
 `;
 
 const useStyles = makeStyles(theme => ({
   formControl: {
-    width: 240,
+    width: 600,
   },
   textField: {
-    width: 240,
+    width: 600,
     marginBottom: '-0.1em',
   },
   box: {
     '& > *': {
-      marginTop: '0.1em',
-      marginBottom: '0.2em',
+      marginTop: '0.3em',
+      marginBottom: '0.3em',
     },
   },
 }));
@@ -113,7 +116,6 @@ const StudentForm = props => {
             >
               <FormControl className={classes.formControl}>
                 <TextField
-                  variant='outlined'
                   placeholder='Location'
                   className={classes.textField}
                 />
@@ -128,9 +130,23 @@ const StudentForm = props => {
                   <MenuItem value=''>
                     <em>None</em>
                   </MenuItem>
-                  <MenuItem value='none'>None</MenuItem>
-                  <MenuItem value='some'>Some</MenuItem>
-                  <MenuItem value='lots'>Lots</MenuItem>
+                  <MenuItem value={1}>
+                    I've taken some online courses
+                  </MenuItem>
+                  <MenuItem value={2}>
+                    I've completed a number of tutorials and built
+                    some personal projects
+                  </MenuItem>
+                  <MenuItem value={3}>
+                    I've completed a coding bootcamp or similar
+                    program
+                  </MenuItem>
+                  <MenuItem value={4}>
+                    I've completed a CS undergraduate degree
+                  </MenuItem>
+                  <MenuItem value={5}>
+                    I'm a professional software developer
+                  </MenuItem>
                 </Select>
               </FormControl>
               <FormControl className={classes.formControl}>
