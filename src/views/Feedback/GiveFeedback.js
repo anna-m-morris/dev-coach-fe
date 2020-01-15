88f30d9ef8be20c75292423b5b2af2bff92ea5fc
BenjaminGrabow <codingbenjamingrabow@gmx.de>
2020-01-15 23:15:22 +0100

    add submit button

--- src/views/Feedback/GiveFeedback.js
+++ src/views/Feedback/GiveFeedback.js
@@ -1,16 +1,25 @@
 import React from 'react';
 import styled from 'styled-components';
-import { getFeedback } from '../../state/actions/feedbackActions';
+// import { getFeedback } from '../../state/actions/feedbackActions';
+import Button from '@material-ui/core/Button';
 import GiveRating from '../../components/DataVisualization/GiveRating';
 import FeedbackInput from '../../components/Inputs/FeedbackInput';
 
-const StyledGiveFeedback = styled.div``;
+const StyledGiveFeedback = styled.div`
+  .button {
+    background-color: #4fad65;
+    font-weight: 200;
+  }
+`;
 
 export default function GiveFeedback() {
   return (
     <StyledGiveFeedback>
       <GiveRating />
       <FeedbackInput />
+      <Button className='button' variant='contained' color='primary'>
+        Submit
+      </Button>
     </StyledGiveFeedback>
   );
 }
