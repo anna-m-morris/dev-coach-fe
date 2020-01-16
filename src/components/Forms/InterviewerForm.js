0429c2a37e8f5c27cb7cad7ade6957108672adbb
BenjaminGrabow <codingbenjamingrabow@gmx.de>
2020-01-16 14:42:23 +0100

    delete all unecessary files

--- src/components/Forms/InterviewerForm.js
+++ src/components/Forms/InterviewerForm.js
@@ -22,7 +22,6 @@ import {
   FormButton,
   InfoParagraph,
 } from './StudentForm';
-
 const CoachCard = styled(StudentCard)`
   width: 50%;
   height: 35em;
@@ -31,43 +30,10 @@ const CoachCard = styled(StudentCard)`
     font-size: 24px;
   }
 `;
-
 const CoachFormContainer = styled(StudentFormContainer)``;
-
 const ThisGreyBackgroundContainer = styled(GreyBackgroundContainer)`
   font-family: 'Ubuntu';
 `;
-const BootstrapInput = withStyles(theme => ({
-  root: {
-    'label + &': {
-      marginTop: theme.spacing(3),
-    },
-  },
-  input: {
-    borderRadius: 4,
-    position: 'relative',
-    backgroundColor: theme.palette.background.paper,
-    border: '1px solid #ced4da',
-    fontSize: 16,
-    padding: '10px 26px 10px 255px',
-    transition: theme.transitions.create([
-      'border-color',
-      'box-shadow',
-    ]),
-    fontFamily: ['ABeeZee'].join(','),
-    '&:focus': {
-      borderRadius: 4,
-      borderColor: '#80bdff',
-      boxShadow: '0 0 0 0.2rem rgba(0,123,255,.25)',
-    },
-  },
-}))(InputBase);
-const useStyles = makeStyles(theme => ({
-  margin: {
-    margin: theme.spacing(1),
-  },
-}));
-
 const useStyles = makeStyles(theme => ({
   formControl: {
     width: 600,
@@ -89,7 +55,6 @@ const useStyles = makeStyles(theme => ({
     paddingBottom: '2em',
   },
 }));
-
 const CoachForm = props => {
   const classes = useStyles();
   const [formValues, setFormValues] = useState({
@@ -229,5 +194,4 @@ const CoachForm = props => {
     </div>
   );
 };
-
 export default connect(state => state, { chooseUserRole })(CoachForm);
