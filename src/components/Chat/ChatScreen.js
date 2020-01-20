12300a5f42723712d27698f6f48dc66a39f5f1ec
BenjaminGrabow <codingbenjamingrabow@gmx.de>
2020-01-20 15:50:33 +0100

    add Messagelist component

--- src/components/Chat/ChatScreen.js
+++ src/components/Chat/ChatScreen.js
@@ -1,11 +1,63 @@
 import React, { Component } from 'react';
+import Chatkit from '@pusher/chatkit-client';
+import MessageList from './MessageList';
 
 class ChatScreen extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      currentUser: {},
+      currentRoom: {},
+      messages: [],
+    };
+  }
+
+  componentDidMount() {
+    const chatManager = new Chatkit.ChatManager({
+      instanceLocator: 'YOUR INSTANCE LOCATOR',
+      userId: this.props.currentUsername,
+      tokenProvider: new Chatkit.TokenProvider({
+        url: 'http://localhost:3001/authenticate',
+      }),
+    });
+
+    chatManager
+      .connect()
+      .then(currentUser => {
+        this.setState({ currentUser });
+        return currentUser.subscribeToRoom({
+          roomId: '031a886d-0f15-4eef-8a83-fc090d56da7f',
+          messageLimit: 100,
+          hooks: {
+            onMessage: message => {
+              this.setState({
+                messages: [...this.state.messages, message],
+              });
+            },
+          },
+        });
+      })
+      .then(currentRoom => {
+        this.setState({ currentRoom });
+      })
+      .catch(error => console.error('error', error));
+  }
+
   render() {
+    const styles = {};
     return (
-      <div>
-        <h1>Chat</h1>
-        <p>Hello, {this.props.currentUsername}</p>
+      <div style={styles.container}>
+        <div style={styles.chatContainer}>
+          <aside style={styles.whosOnlineListContainer}>
+            <h2>Who's online PLACEHOLDER</h2>
+          </aside>
+          <section style={styles.chatListContainer}>
+            <MessageList
+              messages={this.state.messages}
+              style={styles.chatList}
+            />
+          </section>
+        </div>
       </div>
     );
   }
