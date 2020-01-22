54e9c15c7c0e3125e4046cd662e5e3171249e3ba
BenjaminGrabow <codingbenjamingrabow@gmx.de>
2020-01-22 19:16:07 +0100

    Revert "fix import bug"
    This reverts commit 574b016f6610c110440bc9df38b8176a5c0ca6d6.

--- src/views/UserDashboard/CoachDashboard.js
+++ src/views/UserDashboard/CoachDashboard.js
@@ -10,6 +10,7 @@ import {
 } from '../../state/actions/appointmentActions';
 import { saveIdRole } from '../../state/actions/feedbackActions';
 import { startInterview } from '../../state/actions/interviewActions';
+import AppointmentCard from '../../components/Cards/AppointmentCard';
 import EmptyAppointment from '../../components/Cards/EmptyAppointmentCard';
 import NewAppointmentCard from '../../components/Cards/newAppointmentCard';
 import StudentChart from '../../components/DataVisualization/StudentChart';
