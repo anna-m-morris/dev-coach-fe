b9edd24a041931a0cf47b5118e209ba916df50f7
Dom <domeccleston@gmail.com>
2020-01-23 09:51:34 +0000

    refactor and add data viz in again

--- src/views/Feedback/Feedback.js
+++ src/views/Feedback/Feedback.js
@@ -1,14 +1,12 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect } from 'react';
 import { connect } from 'react-redux';
 import styled from 'styled-components';
 import uuid from 'uuid';
-import Pagination from 'antd/lib/pagination';
-import Loader from 'react-loader-spinner';
-
 import { getFeedback } from '../../state/actions/feedbackActions';
 import FeedbackRating from '../../components/DataVisualization/Rating';
 import FeedbackCard from '../../components/Cards/FeedbackCard';
 import EmptyFeedback from '../../components/Cards/EmptyFeedbackCard';
+import StudentChart from '../../components/DataVisualization/StudentChart';
 
 const StyledFeedback = styled.div`
   width: 100%;
@@ -16,85 +14,49 @@ const StyledFeedback = styled.div`
   flex-wrap: wrap;
   justify-content: center;
   margin-top: 1rem;
-
-  .pagination {
-    display: flex;
-    justify-content: center;
-    align-items: center;
+  .chart-container {
+    margin-top: 1.4em;
+    height: 32em;
     width: 100%;
-
-    .ant-pagination-item-active {
-      border-color: #4fad65;
-    }
-
-    .ant-pagination-item-active a {
-      color: #4fad65;
-    }
-  }
-  .loaderStyled {
-    margin-top: 20vh;
+    background: #fff;
+    border-radius: 5px;
+    padding: 1em;
+    box-shadow: 0px 0px 4px rgba(82, 68, 110, 0.3);
+    display: flex;
+    justify-content: space-around;
+    align-items: center;
+    text-align: center;
+    margin-bottom: 2em;
+    color: #4a4a4a;
   }
 `;
 
 const Feedback = ({ user, getFeedback, feedback }) => {
-  const [minValue, setMinValue] = useState(0);
-  const [maxValue, setMaxValue] = useState(6);
-
   useEffect(() => {
     getFeedback(user.id, user.role_id);
   }, [getFeedback, user]);
 
-  const handlePagination = value => {
-    if (value <= 1) {
-      setMinValue(0);
-      setMaxValue(6);
-    } else {
-      setMinValue(value * 6 - 6);
-      setMaxValue(value * 6);
-    }
-  };
-
   return (
-    <StyledFeedback>
-      {feedback ? (
-        <StyledFeedback className='feedback-card-container'>
-          {feedback && feedback.length ? (
-            feedback
-              .slice(minValue, maxValue)
-              .map(feedback => (
-                <FeedbackCard
-                  key={uuid()}
-                  rating={<FeedbackRating rating={feedback.rating} />}
-                  feedback={feedback.feedback}
-                  topic={feedback.appointment_topic}
-                  date={feedback.appointment_datetime.slice(0, 15)}
-                  coachFirstName={feedback.first_name}
-                  coachLastName={feedback.last_name}
-                  avatarUrl={feedback.avatar_url}
-                />
-              ))
-          ) : (
-            <EmptyFeedback />
-          )}
-        </StyledFeedback>
-      ) : (
-        <div className='loaderStyled'>
-          <Loader
-            type='TailSpin'
-            color='#2BAD60'
-            height={80}
-            width={80}
-          />
-        </div>
-      )}
-      <div className='pagination'>
-        <Pagination
-          defaultCurrent={1}
-          defaultPageSize={6}
-          onChange={handlePagination}
-          total={feedback && feedback.length}
-        />
+    <StyledFeedback className='feedback-card-container'>
+      <div className='chart-container'>
+        <StudentChart />
       </div>
+      {feedback && feedback.length ? (
+        feedback.map(feedback => (
+          <FeedbackCard
+            key={uuid()}
+            rating={<FeedbackRating rating={feedback.rating} />}
+            feedback={feedback.feedback}
+            topic={feedback.appointment_topic}
+            date={feedback.appointment_datetime.slice(0, 15)}
+            coachFirstName={feedback.first_name}
+            coachLastName={feedback.last_name}
+            avatarUrl={feedback.avatar_url}
+          />
+        ))
+      ) : (
+        <EmptyFeedback />
+      )}
     </StyledFeedback>
   );
 };
--- src/views/UserDashboard/UserDashboard.js
+++ src/views/UserDashboard/UserDashboard.js
@@ -1,74 +1,77 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import styled from 'styled-components';
 import uuid from 'uuid';
+import styled from 'styled-components';
 import Pagination from 'antd/lib/pagination';
 import 'antd/lib/pagination/style/index.css';
-import Loader from 'react-loader-spinner';
-
 import {
   getAppointment,
   cancelAppointment,
 } from '../../state/actions/appointmentActions';
 import { saveIdRole } from '../../state/actions/feedbackActions';
 import { startInterview } from '../../state/actions/interviewActions';
-
 import EmptyAppointment from '../../components/Cards/EmptyAppointmentCard';
 import NewAppointmentCard from '../../components/Cards/newAppointmentCard';
-
 const StyledContainer = styled.div`
   width: 100%;
   max-width: 1024px;
   margin: 0 auto;
   justify-content: space-between;
-  padding-top: 65px;
-
+  .top-data-card {
+    margin-top: 2em;
+    height: 8em;
+    width: 100%;
+    background: #fff;
+    border-radius: 5px;
+    padding: 1em;
+    box-shadow: 0px 0px 4px rgba(82, 68, 110, 0.3);
+    display: flex;
+    justify-content: space-around;
+    align-items: center;
+    text-align: center;
+    margin-bottom: 2em;
+    color: #4a4a4a;
+    .top-data-section {
+    }
+  }
   .appointments {
     display: flex;
     flex-wrap: wrap;
     justify-content: center;
     align-items: center;
   }
-
   .pagination {
     padding: 2em;
     display: flex;
     justify-content: center;
     align-items: center;
     width: 100%;
-
     .ant-pagination-item-active {
       border-color: #4fad65;
     }
-
     .ant-pagination-item-active a {
       color: #4fad65;
     }
   }
-
   @media (max-width: 768px) {
     max-width: 650px;
     grid-template-columns: repeat(2, 1fr);
   }
-
   @media (max-width: 500px) {
     max-width: 350px;
     grid-template-columns: 1fr;
   }
-
   & > div.appointment-card {
     background: #fff;
     border-radius: 5px;
     padding: 1rem;
     box-shadow: 0px 0px 4px rgba(82, 68, 110, 0.3);
   }
-
   .card-thumbnail {
     height: 80px;
     width: 80px;
     border-radius: 5px;
     position: relative;
-
     img {
       position: absolute;
       height: 100%;
@@ -76,37 +79,28 @@ const StyledContainer = styled.div`
       object-fit: cover;
     }
   }
-
   .card-description {
     margin: 1.5rem 0 0;
     color: #000;
-
     h2,
     h3 {
       font-weight: bold;
     }
-
     h2 {
       font-size: 1.5rem;
       text-transform: uppercase;
     }
-
     h3 {
       font-size: 1.4rem;
     }
-
     p {
       font-weight: 600;
       margin-bottom: 0;
     }
   }
-  .loaderStyled {
-    margin-left: 30rem;
-    margin-top: 20vh;
-  }
 `;
-
 const UserDashboard = props => {
+  console.log(props)
   const {
     appointments,
     getAppointment,
@@ -115,14 +109,11 @@ const UserDashboard = props => {
     startInterview,
     saveIdRole,
   } = props;
-
   const [minValue, setMinValue] = React.useState(0);
   const [maxValue, setMaxValue] = React.useState(6);
-
   React.useEffect(() => {
     setTimeout(() => getAppointment(user.id, user.role_id), 1000);
   }, [getAppointment, user.id, user.role_id]);
-
   const handlePagination = value => {
     if (value <= 1) {
       setMinValue(0);
@@ -132,61 +123,68 @@ const UserDashboard = props => {
       setMaxValue(value * 6);
     }
   };
+  const calculateFormattedMean = arr => {
+    const sum = arr.reduce((a, c) => ({
+      rating: a.rating + c.rating,
+    }));
+    return (sum.rating / arr.length).toString().slice(0, 4);
+  };
   return (
     <StyledContainer>
-      {appointments ? (
-        <StyledContainer>
-          {appointments && appointments.length ? (
-            <div className='appointments'>
-              {appointments
-                .slice(minValue, maxValue)
-                .map(appointment => (
-                  <NewAppointmentCard
-                    key={uuid()}
-                    appointment={appointment}
-                    cancel={() => cancelAppointment(appointment.id)}
-                    startInterview={() =>
-                      startInterview(appointment.user_id, props)
-                    }
-                    saveIdRole={() =>
-                      saveIdRole(appointment.role_id, appointment.id)
-                    }
-                  />
-                ))}
-              <div className='pagination'>
-                <Pagination
-                  defaultCurrent={1}
-                  defaultPageSize={6}
-                  onChange={handlePagination}
-                  total={appointments.length}
-                />
-              </div>
-            </div>
-          ) : (
-            <EmptyAppointment />
-          )}
-        </StyledContainer>
-      ) : (
-        <div className='loaderStyled'>
-          <Loader
-            type='TailSpin'
-            color='#2BAD60'
-            height={80}
-            width={80}
-          />
+      <>
+        <div className='top-data-card'>
+          <div className='top-data-section'>
+            <h3>Average rating:</h3>
+            <h3>{props.feedback ? `${calculateFormattedMean(props.feedback)}` : 'N/A'}</h3>
+          </div>
+          <div className='top-data-section'>
+            <h3>Number of interviews completed:</h3>
+            <h3>{props.feedback ? props.feedback.length : 'N/A'}</h3>
+          </div>
+          <div className='top-data-section'>
+            <h3>Upcoming interviews:</h3>
+            <h3>{props.appointments ? props.appointments.length : 'N/A'}</h3>
+          </div>
         </div>
-      )}
+        {appointments && appointments.length ? (
+          <div className='appointments'>
+            {appointments
+              .slice(minValue, maxValue)
+              .map(appointment => (
+                <NewAppointmentCard
+                  appointment={appointment}
+                  cancel={() => cancelAppointment(appointment.id)}
+                  startInterview={() =>
+                    startInterview(appointment.user_id, props)
+                  }
+                  saveIdRole={() =>
+                    saveIdRole(appointment.role_id, appointment.id)
+                  }
+                />
+              ))}
+            <div className='pagination'>
+              <Pagination
+                defaultCurrent={1}
+                defaultPageSize={6}
+                onChange={handlePagination}
+                total={appointments.length}
+              />
+            </div>
+          </div>
+        ) : (
+          <EmptyAppointment />
+        )}
+      </>
     </StyledContainer>
   );
 };
-
 const mapStateToProps = state => {
   return {
     user: state.userReducer.user,
     appointments: state.appointmentsReducer.appointments,
+    feedback: state.feedbackReducer.feedback,
   };
 };
-
 export default connect(mapStateToProps, {
   getAppointment,
   cancelAppointment,
