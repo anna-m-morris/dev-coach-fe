670be87cd65457f6135535e65edbd93d6bf33999
Liam Sutton <devliam90@gmail.com>
2020-01-24 15:39:14 +0000

    add Link to join now button

--- src/components/Landing/Footer/Footer.js
+++ src/components/Landing/Footer/Footer.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import { StyledButton, buttonTheme } from '../Landing-styles';
 import { FooterContainer } from './Footer-styles';
 
@@ -15,9 +16,9 @@ const Footer = () => (
         <h2>What are you waiting for?</h2>
       </div>
       <div className='cta-button'>
-        <a href='/register'>
+        <Link to='/register'>
           <StyledButton theme={buttonTheme}>Join Now</StyledButton>
-        </a>
+        </Link>
       </div>
     </div>
     <div className='footer-bottom'>
