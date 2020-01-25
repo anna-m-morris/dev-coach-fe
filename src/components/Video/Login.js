96f5dabe28aea1cbcbfb369a428a0e3aeebe5e06
BenjaminGrabow <codingbenjamingrabow@gmx.de>
2020-01-25 01:40:57 +0100

    add arrayBufferToString file

--- src/components/Video/Login.js
+++ src/components/Video/Login.js
@@ -1,24 +1,23 @@
-import React, { Component } from "react";
+import React, { Component } from 'react';
 import { Container, Row, Col, Form, Button } from 'react-bootstrap';
 import axios from 'axios';
-import Pusher from "pusher-js";
+import Pusher from 'pusher-js';
 
 import uniquename from '../helpers/uniquename';
-import stringHash from "string-hash";
+import stringHash from 'string-hash';
 
 const channel_name = uniquename();
 
 const PUSHER_APP_KEY = process.env.REACT_APP_PUSHER_APP_KEY;
 const PUSHER_APP_CLUSTER = process.env.REACT_APP_PUSHER_APP_CLUSTER;
-const BASE_URL = "Heroku"; // replace this later
+const BASE_URL = 'Heroku'; // replace this later
 
 class LoginScreen extends Component {
-
   state = {
     username: '',
     channel: channel_name,
-    isLoading: false
-  }
+    isLoading: false,
+  };
 
   constructor(props) {
     super(props);
@@ -29,77 +28,93 @@ class LoginScreen extends Component {
   render() {
     return (
       <Container>
-        <Row className="justify-content-md-center">
-          <Col md="4"></Col>
+        <Row className='justify-content-md-center'>
+          <Col md='4'></Col>
           <Col md={4}>
             <Form>
               <Form.Group>
                 <Form.Label>Username</Form.Label>
-                <Form.Control type="text" placeholder="username" value={this.state.username} onChange={this.onTypeText} />
+                <Form.Control
+                  type='text'
+                  placeholder='username'
+                  value={this.state.username}
+                  onChange={this.onTypeText}
+                />
               </Form.Group>
 
               <Form.Group>
                 <Form.Label>Channel</Form.Label>
-                <Form.Control type="text" placeholder="channel" value={this.state.channel} onChange={this.onTypeText} />
-                <Form.Text className="text-muted">
-                  This is the name of your channel. Replace this if you want to connect to an existing channel.
+                <Form.Control
+                  type='text'
+                  placeholder='channel'
+                  value={this.state.channel}
+                  onChange={this.onTypeText}
+                />
+                <Form.Text className='text-muted'>
+                  This is the name of your channel. Replace this if
+                  you want to connect to an existing channel.
                 </Form.Text>
               </Form.Group>
 
-              <Button variant="primary" type="button" onClick={this.login} disabled={this.state.isLoading}>
+              <Button
+                variant='primary'
+                type='button'
+                onClick={this.login}
+                disabled={this.state.isLoading}
+              >
                 {this.state.isLoading ? 'Logging in…' : 'Login'}
               </Button>
             </Form>
           </Col>
 
-          <Col md="4"></Col>
+          <Col md='4'></Col>
         </Row>
       </Container>
     );
   }
 }
 
-onTypeText = (evt) => {
+onTypeText = evt => {
   const field = evt.target.getAttribute('placeholder');
   this.setState({
-    [field]: evt.target.value
+    [field]: evt.target.value,
   });
-}
+};
 
 login = async () => {
   const { username, channel } = this.state;
   const user_id = stringHash(username).toString();
 
   this.setState({
-    isLoading: true
+    isLoading: true,
   });
 
   this.pusher = new Pusher(PUSHER_APP_KEY, {
     authEndpoint: `${BASE_URL}/pusher/auth`,
     cluster: PUSHER_APP_CLUSTER,
-    encrypted: true
+    encrypted: true,
   });
 
-  this.my_channel = this.pusher.subscribe(`private-user-${username}`); 
-  this.my_channel.bind("pusher:subscription_error", (status) => {
-    console.log("error subscribing to channel: ", status);
+  this.my_channel = this.pusher.subscribe(`private-user-${username}`);
+  this.my_channel.bind('pusher:subscription_error', status => {
+    console.log('error subscribing to channel: ', status);
   });
 
   // next: add code when subscription succeeded
-  this.my_channel.bind("pusher:subscription_succeeded", async () => {
-    console.log("subscription to own channel succeeded");
+  this.my_channel.bind('pusher:subscription_succeeded', async () => {
+    console.log('subscription to own channel succeeded');
     try {
       const response = await axios.post(`${BASE_URL}/login`, {
         user_id,
         username,
-        channel
+        channel,
       });
 
       if (response.statusText === 'OK') {
         this.setState({
           isLoading: false,
           username: '',
-          channel: ''
+          channel: '',
         });
 
         // navigate to the group chat page with all the relevant data
@@ -107,14 +122,13 @@ login = async () => {
           username,
           channel,
           pusher: this.pusher,
-          my_channel: this.my_channel
+          my_channel: this.my_channel,
         });
       }
-
     } catch (e) {
-      console.log("error occured logging in: ", e);
+      console.log('error occured logging in: ', e);
     }
   });
-}
+};
 
 export default LoginScreen;
--- src/components/Video/uniquename.js
+++ src/components/Video/uniquename.js
@@ -1,10 +1,10 @@
-const generateName = require("sillyname");
+const generateName = require('sillyname');
 
 const generateUsername = () => {
   const min = 10;
   const max = 99;
   const number = Math.floor(Math.random() * (max - min + 1)) + min;
-  const username = generateName().replace(" ", "_") + number;
+  const username = generateName().replace(' ', '_') + number;
   return username;
 };
 
