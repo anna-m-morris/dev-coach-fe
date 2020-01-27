2997e526e97c1d8c4ffdfea02fd1437bfa549a25
funmi7 <talabifunmilayo@gmail.com>
2020-01-27 13:25:01 +0100

    Added active class styling

--- src/components/Chat/UserList.js
+++ src/components/Chat/UserList.js
@@ -1,40 +1,41 @@
-import React from 'react';
+import React, { useState } from 'react';
 import uuid from 'uuid';
 import styled from 'styled-components';
 
 const UserListStyle = styled.div`
+  position: static;
+
   .bg {
     border-top: 1px solid #ced4da;
     padding: 1rem;
     border-bottom: none;
     cursor: pointer;
+  }
 
-    &:hover {
-      background: #4fad65;
-      color: white;
-      padding: 1rem;
-      border-radius: 5px;
-    }
+  .text {
+    width: 70%;
+    height: 70%;
+    margin-top: -20px;
+    text-align: center;
+    margin-left: 3rem;
   }
 
   .bgClicked {
-    background: white;
+    background: #4fad65;
     border-top: 1px solid #ced4da;
     padding: 1rem;
     border-bottom: none;
     cursor: pointer;
-
-    &:hover {
-      background: #4fad65;
-      color: white;
-      padding: 1rem;
-      border-radius: 5px;
-    }
+    color: white;
   }
 `;
 
 const UserList = props => {
   const { user, rooms, startChat } = props;
+  const [clickedIndex, setClickedindex] = useState(0);
+  const handleClick = index => {
+    setClickedindex(index);
+  };
 
   return (
     <>
@@ -45,14 +46,29 @@ const UserList = props => {
             onClick={() => startChat(room.id)}
           >
             {' '}
-            <div className='bg'>
-              {user.role_id === 2
-                ? room.custom_data.role_id_one
-                : room.custom_data.role_id_two}
-            </div>
+            <ChangeStyle
+              clicked={room === clickedIndex}
+              onClick={() => handleClick(room)}
+              userRole={
+                user.role_id === 2
+                  ? room.custom_data.role_id_one
+                  : room.custom_data.role_id_two
+              }
+            />
           </UserListStyle>
         ))}
     </>
+  );
+};
+
+const ChangeStyle = props => {
+  return (
+    <div
+      className={props.clicked ? 'bgClicked' : 'bg'}
+      onClick={props.onClick}
+    >
+      {props.userRole}
+    </div>
   );
 };
 
