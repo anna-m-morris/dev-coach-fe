eaf004db965744fdd02d09f07abb441e516353be
funmi7 <talabifunmilayo@gmail.com>
2020-01-30 16:36:19 +0100

    Adding responsiveness to faqPage

--- src/components/FAQ/FaqStyles.js
+++ src/components/FAQ/FaqStyles.js
@@ -1,4 +1,5 @@
 import styled from 'styled-components';
+import devices from '../../utils/devices';
 
 export const BodyDiv = styled.div`
   height: 60vh;
@@ -16,19 +17,26 @@ export const FAQContainer = styled.div`
 export const AskedQuestionDiv = styled.div`
   margin-bottom: 20px;
   max-width: 100%;
-  width: 32em;
+  width: 49rem;
   border-color: #d2d4dc;
   border-radius: 10px;
   height: 50px;
   background-color: #fff;
   padding: 5px 10px;
-  font-size: 24px;
+  font-size: 4rem;
   box-shadow: 0px 6px 10px rgba(0, 0, 0, 0.15);
   transition: box-shadow 0.2s;
   display: flex;
   align-items: center;
   cursor: pointer;
 
+  @media ${devices.tablet} {
+    width: 25rem;
+  }
+
+  @media ${devices.mobile} {
+    width: 17rem;
+  }
   p {
     font-size: 19px;
     font-weight: 600;
