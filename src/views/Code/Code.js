3132325800e43144cb79fae229002daba12d98c5
Dom <domeccleston@gmail.com>
2020-01-30 09:12:18 +0000

    start adding in code view

---
+++ src/views/Code/Code.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import styled from 'styled-components';
+import Display from './components/Display';
+import Editor from './components/Editor';
+
+const FlexContainer = styled.div`
+  display: flex;
+  align-items: center;
+  width: 100%;
+  height: 100%;
+`;
+
+function App() {
+  const [output, setOutput] = React.useState('');
+
+  return (
+    <div className='App'>
+      <FlexContainer>
+        <Editor output={output} setOutput={setOutput} />
+        <Display initialText='$  ' output={output} />
+      </FlexContainer>
+    </div>
+  );
+}
+
+export default App;
