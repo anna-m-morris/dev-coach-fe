574c497b470c6add6751b3f959ce21d2b3f32794
Dom <domeccleston@gmail.com>
2020-01-30 10:15:01 +0000

    Add in interface for coder component

--- src/components/Code/Interface.js
+++ src/components/Code/Interface.js
@@ -1,1 +1,39 @@
-import 
+import React from 'react';
+import { connect } from 'react-redux';
+import {
+  Button,
+  FormControl,
+  Select,
+  InputLabel,
+  MenuItem,
+} from '@material-ui/core';
+
+// todos:
+
+const handlePost = () => {};
+const handleSelection = () => {};
+const language = {};
+
+const Interface = () => {
+  return (
+    <div>
+      <h1>Code Editor</h1>
+      <Button onClick={handlePost}>Submit</Button>
+      <FormControl>
+        <InputLabel>Select Programming Language</InputLabel>
+        <Select
+          style={{ width: '20em' }}
+          value={language}
+          onChange={handleSelection}
+        >
+          <MenuItem value='javascript'>Javascript</MenuItem>
+          <MenuItem value='python'>Python</MenuItem>
+          <MenuItem value='java'>Java</MenuItem>
+          <MenuItem value='cpp'>C++</MenuItem>
+        </Select>
+      </FormControl>
+    </div>
+  );
+};
+
+export default connect(state => state)(Interface);
