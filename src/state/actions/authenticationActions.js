ad1726a566a99b0d35d671a15be930a1d2478e35
Liam Sutton <devliam90@gmail.com>
2020-01-31 13:48:09 +0000

    error handling for email and passwords

--- src/state/actions/authenticationActions.js
+++ src/state/actions/authenticationActions.js
@@ -2,7 +2,8 @@ import axios from 'axios';
 import axiosWithAuth from '../../utils/axiosWithAuth';
 import * as types from './actionTypes';
 
-const url = process.env.REACT_APP_BASE_URL;
+// const url = process.env.REACT_APP_BASE_URL;
+const url = 'http://localhost:5000/';
 
 export const sendResetPasswordEmail = (
   props,
@@ -70,7 +71,7 @@ export const login = (props, values) => dispatch => {
     .catch(err => {
       dispatch({
         type: types.LOGIN_ERROR,
-        payload: err.data,
+        payload: err.response.data.message,
       });
     });
 };
