46e44f17530e9c921eea01fc1f4d7bc850d8d0c5
Liam Sutton <devliam90@gmail.com>
2020-02-03 16:44:36 +0000

    spacing, background reset password

--- src/components/Onboarding/ResetPasswordForm.js
+++ src/components/Onboarding/ResetPasswordForm.js
@@ -10,6 +10,7 @@ import {
   buttonTheme,
   Logo,
 } from '../Landing/Landing-styles';
+import { LoginContainer } from './Login/LoginStyles';
 import {
   showErrorMessage,
   showSuccessMessage,
@@ -18,6 +19,23 @@ import {
 
 import Notification from '../Notifications/Notification';
 import { sendResetPasswordEmail } from '../../state/actions/authenticationActions';
+
+const ResetContainer = styled(LoginContainer)`
+  justify-content: center;
+
+  .form-card-container {
+    padding: 1rem;
+  }
+
+  .form-button-container {
+    width: 100%;
+    margin-top: 0.5rem;
+    .form-button {
+      width: 100%;
+      border-radius: 2rem;
+    }
+  }
+`;
 
 const ResetPasswordForm = props => {
   const {
@@ -58,53 +76,54 @@ const ResetPasswordForm = props => {
     );
   };
   return (
-    <div>
-      <GreyBackgroundContainer>
-        <Notification
-          onClose={closeMessage}
-          variant='success'
-          message='password reset email sent successfully'
-          open={success}
-        />
-        <Notification
-          onClose={closeMessage}
-          variant='error'
-          message='That email address is not recognized. Please try again'
-          open={error}
-        />
-        <FormCard>
-          <Link to='/'>
-            <Logo />
-          </Link>
-          <h3>Enter your email to reset your password</h3>
-          <FormContainer>
-            <Form>
-              <div>
-                <Field
-                  onChange={handleChange}
-                  value={resetUser.email}
-                  type='email'
-                  name='email'
-                  placeholder='enter your email address'
-                />
-                {errors.email && touched.email && (
-                  <StyledError>{errors.email}</StyledError>
-                )}
-
-                <StyledResetButton
+    <ResetContainer className='reset-container'>
+      <Notification
+        onClose={closeMessage}
+        variant='success'
+        message='password reset email sent successfully'
+        open={success}
+      />
+      <Notification
+        onClose={closeMessage}
+        variant='error'
+        message='That email address is not recognized. Please try again'
+        open={error}
+      />
+      <div className='form-card-container'>
+        <Link to='/'>
+          <Logo />
+        </Link>
+        <h3>Enter your email to reset your password</h3>
+        <div className='form-container'>
+          <Form className='form'>
+            <div className='input-container'>
+              <Field
+                className='form-input'
+                onChange={handleChange}
+                value={resetUser.email}
+                type='email'
+                name='email'
+                placeholder='enter your email address'
+              />
+              {errors.email && touched.email && (
+                <p className='error'>{errors.email}</p>
+              )}
+              <div className='form-button-container'>
+                <StyledButton
+                  className='form-button'
                   onClick={handleSubmit}
                   theme={buttonTheme}
                   type='submit'
                   disabled={isSubmitting}
                 >
                   Send email -->
-                </StyledResetButton>
+                </StyledButton>
               </div>
-            </Form>
-          </FormContainer>
-        </FormCard>
-      </GreyBackgroundContainer>
-    </div>
+            </div>
+          </Form>
+        </div>
+      </div>
+    </ResetContainer>
   );
 };
 
@@ -141,85 +160,85 @@ export default connect(mapStateToProps, {
   sendResetPasswordEmail,
 })(FormikResetPasswordForm);
 
-export const StyledResetButton = styled(StyledButton)`
-  margin-top: 20px;
-`;
+// export const StyledResetButton = styled(StyledButton)`
+//   margin-top: 20px;
+// `;
 
-export const GreyBackgroundContainer = styled.div`
-  height: 100vh;
-  background: #fff;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  background-color: #11aa44;
-`;
+// export const GreyBackgroundContainer = styled.div`
+//   height: 100vh;
+//   background: #fff;
+//   display: flex;
+//   justify-content: center;
+//   align-items: center;
+//   background-color: #11aa44;
+// `;
 
-export const FormCard = styled.div`
-  background: white;
-  height: 20em;
-  width: 25em;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  box-shadow: 0px 4px 6px rgba(0, 0, 0, 0.2);
-  border-radius: 6px;
+// export const FormCard = styled.div`
+//   background: white;
+//   height: 20em;
+//   width: 25em;
+//   display: flex;
+//   flex-direction: column;
+//   align-items: center;
+//   box-shadow: 0px 4px 6px rgba(0, 0, 0, 0.2);
+//   border-radius: 6px;
 
-  h3 {
-    color: #292d38;
-    margin-top: 0.5rem;
-  }
-`;
+//   h3 {
+//     color: #292d38;
+//     margin-top: 0.5rem;
+//   }
+// `;
 
-export const FormContainer = styled.div`
-  height: 30%;
-  width: 100%;
+// export const FormContainer = styled.div`
+//   height: 30%;
+//   width: 100%;
 
-  form {
-    width: 100%;
-    height: 95%;
-    display: flex;
-    flex-direction: column;
-    justify-content: space-evenly;
-    align-items: center;
+//   form {
+//     width: 100%;
+//     height: 95%;
+//     display: flex;
+//     flex-direction: column;
+//     justify-content: space-evenly;
+//     align-items: center;
 
-    div {
-      width: 70%;
-      height: 60px;
-      align-self: center;
-    }
-  }
+//     div {
+//       width: 70%;
+//       height: 60px;
+//       align-self: center;
+//     }
+//   }
 
-  input {
-    align-self: center;
-    background: #f7f7f7;
-    height: 2em;
-    width: 92%;
-    font-family: Ubuntu, sans-serif;
-    border-radius: 4px;
-    border: 1px solid #c8c8c8;
-    padding: 0.5em;
-    font-size: 16px;
-    color: #808080;
-    transition: ease-out 0.1s;
+//   input {
+//     align-self: center;
+//     background: #f7f7f7;
+//     height: 2em;
+//     width: 92%;
+//     font-family: Ubuntu, sans-serif;
+//     border-radius: 4px;
+//     border: 1px solid #c8c8c8;
+//     padding: 0.5em;
+//     font-size: 16px;
+//     color: #808080;
+//     transition: ease-out 0.1s;
 
-    :hover {
-      background: #ffffff;
-      transition: ease-in 0.1s;
-    }
+//     :hover {
+//       background: #ffffff;
+//       transition: ease-in 0.1s;
+//     }
 
-    :focus {
-      outline: none;
-    }
-  }
+//     :focus {
+//       outline: none;
+//     }
+//   }
 
-  button {
-    width: 98%;
-  }
-`;
+//   button {
+//     width: 98%;
+//   }
+// `;
 
-const StyledError = styled.p`
-  padding: 0;
-  margin: 0;
-  color: red;
-  font-size: 0.8rem;
-`;
+// const StyledError = styled.p`
+//   padding: 0;
+//   margin: 0;
+//   color: red;
+//   font-size: 0.8rem;
+// `;
