8dcac479e58f126f93cd197ecb354d5cd3825332
Liam Sutton <devliam90@gmail.com>
2020-02-03 23:16:24 +0000

    center login form

--- src/components/Onboarding/Login/LoginStyles.js
+++ src/components/Onboarding/Login/LoginStyles.js
@@ -7,10 +7,13 @@ export const LoginContainer = styled.div`
   flex-direction: column;
   align-items: center;
 
+  .navigation-container {
+    width: 100%;
+  }
+
   .navigation {
     width: 100%;
     box-shadow: 0px 4px 7px rgba(0, 0, 0, 0.1);
-    margin-bottom: 3rem;
 
     .logo {
       padding: 0;
@@ -23,6 +26,14 @@ export const LoginContainer = styled.div`
         opacity: none;
       }
     }
+  }
+
+  .content-container {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    width: 100%;
+    height: 100%;
   }
 
   .form-card-container {
