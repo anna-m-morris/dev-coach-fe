83d15904f4397f54582e175e2164e45fa54633d1
Liam Sutton <devliam90@gmail.com>
2020-02-04 20:54:28 +0000

    remove unused logos

---
+++ src/components/About/About.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Navigation from '../Landing/Navigation/Navigation';
+
+const About = () => {
+  return (
+    <div className='about-container'>
+      <Navigation />
+    </div>
+  );
+};
+
+export default About;
