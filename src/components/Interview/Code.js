a45e1ffea04b9386cc8710bd0c788c566f3f9653
Dom <domeccleston@gmail.com>
2020-02-04 15:18:35 +0000

    keyboard shortcut working

--- src/components/Interview/Code.js
+++ src/components/Interview/Code.js
@@ -249,7 +249,7 @@ class Code extends Component {
       .catch(err => {});
   };
 
-  handlekeydownEvent(event) {
+  handlekeydownEvent = (event) => {
     if (event.keyCode === 13 && event.ctrlKey) {
       this.logCode();
     }
