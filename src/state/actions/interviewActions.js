3f163a8dc4d5541a21ac2b75d87f283ea35f5b8a
BenjaminGrabow <codingbenjamingrabow@gmx.de>
2020-02-05 15:34:40 +0100

    create finishInterview action

--- src/state/actions/interviewActions.js
+++ src/state/actions/interviewActions.js
@@ -1,7 +1,27 @@
+import axiosWithAuth from '../../utils/axiosWithAuth';
+
 export const VIDEO_SUCCESSFUL = 'VIDEO_SUCCESSFUL';
+export const FINISH_INTERVIEW_SUCCESS = 'FINISH_INTERVIEW_SUCCESS';
+export const FINISH_INTERVIEW_ERROR = 'FINISH_INTERVIEW_ERROR';
+
+const url = process.env.REACT_APP_BASE_URL;
 
 export const startInterview = (peerId, props) => {
   props.history.push('/interview');
 
   return { type: VIDEO_SUCCESSFUL, payload: peerId };
+};
+
+export const finishInterview = appointment_id => dispatch => {
+debugger
+  axiosWithAuth()
+    .put(`${url}appointment/${appointment_id}`)
+    .then(res => {
+      dispatch({
+        type: FINISH_INTERVIEW_SUCCESS,
+      });
+    })
+    .catch(err => {
+      dispatch({ type: FINISH_INTERVIEW_ERROR, payload: err });
+    });
 };
