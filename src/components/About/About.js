e1a6df8ad1cc995ff59a0a6ffe7a989fddd7b817
Liam Sutton <devliam90@gmail.com>
2020-02-05 10:58:33 +0000

    import pics

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,6 +1,12 @@
 import React from 'react';
 import Navigation from '../Landing/Navigation/Navigation';
 import AboutContainer from './AboutStyles';
+import ben from './img/ben.jpeg';
+import ola from './img/ola.jpeg';
+import jayne from './img/jayne.jpeg';
+import dom from './img/dom.jpeg';
+import funmi from './img/funmi.jpeg';
+import me from './img/me.jpg';
 
 const About = () => {
   return (
