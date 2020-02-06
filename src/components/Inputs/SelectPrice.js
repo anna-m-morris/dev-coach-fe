db3f583f31c96b9c77da3690fc8870ad895a08c6
funmi7 <talabifunmilayo@gmail.com>
2020-02-06 17:46:07 +0100

    Trying to change style to search

--- src/components/Inputs/SelectPrice.js
+++ src/components/Inputs/SelectPrice.js
@@ -1,15 +1,61 @@
 import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
+import styled from 'styled-components';
+// import { makeStyles } from '@material-ui/core/styles';
+// import InputLabel from '@material-ui/core/InputLabel';
+// import Input from '@material-ui/core/Input';
+// import MenuItem from '@material-ui/core/MenuItem';
+// import FormControl from '@material-ui/core/FormControl';
+// import Select from '@material-ui/core/Select';
+import { makeStyles, withStyles } from '@material-ui/core/styles';
 import InputLabel from '@material-ui/core/InputLabel';
-import Input from '@material-ui/core/Input';
 import MenuItem from '@material-ui/core/MenuItem';
 import FormControl from '@material-ui/core/FormControl';
 import Select from '@material-ui/core/Select';
+import NativeSelect from '@material-ui/core/NativeSelect';
+import InputBase from '@material-ui/core/InputBase';
+
+// const SelectPriceStyle = styled.div`
+//   border: 1px solid #ced4da;
+// `;
+
+// const useStyles = makeStyles(theme => ({
+//   formControl: {
+//     margin: theme.spacing(1),
+//     minWidth: 120,
+//     border: '1px solid #ced4da',
+//     borderRadius: '10rem',
+//   },
+// }));
+
+const BootstrapInput = withStyles(theme => ({
+  root: {
+    'label + &': {
+      marginTop: theme.spacing(3),
+    },
+  },
+  input: {
+    minWidth: 120,
+    borderRadius: 8,
+    position: 'relative',
+    // backgroundColor: theme.palette.background.paper,
+    border: '1px solid #ced4da',
+    fontSize: 16,
+    padding: '10px 26px 10px 12px',
+    transition: theme.transitions.create([
+      'border-color',
+      'box-shadow',
+    ]),
+    '&:focus': {
+      borderRadius: 4,
+      borderColor: '#80bdff',
+      boxShadow: '0 0 0 0.2rem rgba(0,123,255,.25)',
+    },
+  },
+}))(InputBase);
 
 const useStyles = makeStyles(theme => ({
-  formControl: {
+  margin: {
     margin: theme.spacing(1),
-    minWidth: 120,
   },
 }));
 
@@ -18,12 +64,12 @@ const SelectPrice = props => {
 
   return (
     <div>
-      <FormControl className={classes.formControl}>
+      <FormControl variant='none' className={classes.margin}>
         <InputLabel htmlFor='grouped-select'>Price</InputLabel>
         <Select
           defaultValue=''
           input={
-            <Input
+            <BootstrapInput
               onChange={e => props.searchForPrice(e.target.value)}
               name='price'
               id='price-select'
