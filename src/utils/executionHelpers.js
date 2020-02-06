7bd2124551b33221fc45341417d97dc628725054
BenjaminGrabow <codingbenjamingrabow@gmx.de>
2020-02-06 22:51:46 +0100

    add collatzSequenze with two testcases

--- src/utils/executionHelpers.js
+++ src/utils/executionHelpers.js
@@ -245,7 +245,8 @@ export const testDataObj = {
       },
     ],
   },
-  romanNumeralize: `function romanNumeralize(n) {
+  romanNumeralize: {
+    state: `function romanNumeralize(n) {
     // Define a function that takes in a positive integer
     // and returns the Roman Numeral representation of that number.  
 
@@ -266,11 +267,74 @@ export const testDataObj = {
   
   // Example: romanNumeralize(1973) should return 'MCMLXXIII'.
 }`,
-  testData: [
-    { testCase: 1973, testResult: 'MCMLXXIII' },
-    { testCase: 2593, testResult: 'MMDXCIII' },
-    { testCase: 3123, testResult: 'MMMCXXIII' },
-  ],
+    testData: [
+      { testCase: 1973, testResult: 'MCMLXXIII' },
+      { testCase: 2593, testResult: 'MMDXCIII' },
+      { testCase: 3123, testResult: 'MMMCXXIII' },
+    ],
+  },
+  collatzSequence: {
+    state: `function collatzSequence(num){
+    // Write a function which takes a positive integer number as an argument and 
+    // returns it's "Collatz chain".The Collatz chain will stop at one. Named 
+    // after Lothar Collatz, the "Collatz conjecture" defines a sequence of numbers.
+    // That sequence is the Collatz "chain". Starting with a positive integer,
+    // the Collatz conjecture determines the next integer in the chain until 
+    // the number 1 is obtained.Your Collatz algorithm will evaluate the integer
+    // and then, depending on the condition of the integer, perform the following tasks:
+    // If the integer is even, then halve the number.
+    // If the integer is not even, then multiply it by 3 and add one.
+    // An example chain starting from the number 23 looks like this:
+    // [23, 70, 35, 106, 53, 160, 80, 40, 20, 10, 5, 16, 8, 4, 2, 1]
+    // Boldface signifies the odd numbers.
+    }`,
+    testData: [
+      {
+        testCase: 23,
+        testResult: [
+          23,
+          70,
+          35,
+          106,
+          53,
+          160,
+          80,
+          40,
+          20,
+          10,
+          5,
+          16,
+          8,
+          4,
+          2,
+          1,
+        ],
+      },
+      {
+        testCase: 44,
+        testResult: [
+          44,
+          22,
+          11,
+          34,
+          17,
+          52,
+          26,
+          13,
+          40,
+          20,
+          10,
+          5,
+          16,
+          8,
+          4,
+          2,
+          1,
+        ],
+      },
+      { testCase: 87, testResult: [] },
+    ],
+  },
 };
 
 export const invokeCode = (code, testCase, value, language) => {
