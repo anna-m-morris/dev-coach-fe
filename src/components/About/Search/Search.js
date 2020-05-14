91a81b48de7f5d9322a0f8914a4fc5f70a2507ea
Anna Morris <annapatton444@gmail.com>
2020-05-13 21:09:43 -0500

    about/search text

---
+++ src/components/About/Search/Search.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const Search = () => {
+  return (
+    <div>
+      <h1>One-On-One Coaching</h1>
+      <h2>
+        From beginners making a splash in the industry to senior
+        engineers, our coaches are ready to help you level up your
+        skills.
+      </h2>
+      <p>Live chat, code along, interveiw practice, and more!</p>
+      <div>
+        {/* search bar */}
+      </div>
+    </div>
+  );
+};
+
+export default Search;
