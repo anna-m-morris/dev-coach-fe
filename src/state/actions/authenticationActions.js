01a72a9b59cac14d650628955dbcb42d84245dcf
Dallas James <dallasjames42@gmail.com>
2020-06-25 20:07:29 -0600

    auth fix

--- src/state/actions/authenticationActions.js
+++ src/state/actions/authenticationActions.js
@@ -1,8 +1,9 @@
-import axios from 'axios';
+// import axios from 'axios';
 import axiosWithAuth from '../../utils/axiosWithAuth';
 import * as types from './actionTypes';
+import axios from '../../utils/axiosWithoutAuth'
 
-const url = process.env.LOCAL_BACKEND_SERVER;
+const url = process.env.REACT_APP_BASE_URL;
 
 export const sendResetPasswordEmail = (
   props,
@@ -11,8 +12,8 @@ export const sendResetPasswordEmail = (
   showError,
   closeMessage,
 ) => dispatch => {
-  axios
-    .post(`${url}user/resetPassword`, userInfo)
+  axios()
+    .post(`user/resetPassword`, userInfo)
     .then(res => {
       if (
         res.data.message ===
@@ -56,10 +57,10 @@ export const saveRoleId = (handleNext, role) => dispatch => {
 
 export const login = (props, values) => dispatch => {
   dispatch({ type: types.LOGIN_START });
-  axios
-    .post(`${url}user/login`, values)
+  axios()
+    .post(`user/login`, values)
     .then(res => {
-      console.log('login.res', res);
+      console.log("login.res",res)
       localStorage.setItem('token', res.data.token);
       setTimeout(
         () =>
@@ -74,16 +75,19 @@ export const login = (props, values) => dispatch => {
     .catch(err => {
       dispatch({
         type: types.LOGIN_ERROR,
-        payload: err.response.data.message,
+        payload: err.message,
       });
     });
 };
 
 export const register = (props, values) => dispatch => {
+  console.log(url)
   dispatch({ type: types.SIGN_UP_START });
-  axios
-    .post(`${url}user/register`, values)
+  console.log(props)
+  axios()
+    .post(`user/register`, values)
     .then(res => {
+      console.log("register.res",res)
       dispatch({
         type: types.SIGN_UP_SUCCESSFUL,
         payload: res.data.user,
@@ -95,7 +99,7 @@ export const register = (props, values) => dispatch => {
     .catch(err => {
       dispatch({
         type: types.SIGN_UP_ERROR,
-        payload: err.response.data.message,
+        payload: err.message,
       });
     });
 };
